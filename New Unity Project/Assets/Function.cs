1f97882d5cb3d5c41da52bc5cda2fa9e7b93869f
Unknown <jameson.ballard21@gmail.com>
2016-09-16 12:41:00 -0600

    Functions

---
+++ New Unity Project/Assets/Function.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Function : MonoBehaviour {
+
+    // It seems that when you make a function outside of Start or Update that it needs to be called in
+    // either Start or Update. I am guessing that Start and Update do not need to be called themselves
+    // because Start calls itself at the beginning of the game and Update is called automatically every
+    // frame.
+
+    int Pie = 12;
+
+	void Start () {
+        PieDisplay();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void PieDisplay ()
+    {
+        print("For the love of" + Pie);
+    }
+}
