28212199ae3ce7921f3b85d5ba35781584ff056d
Unknown <jameson.ballard21@gmail.com>
2016-09-17 16:29:34 -0600

    Conventions&Syntax

---
+++ New Unity Project/Assets/Syntax.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Syntax : MonoBehaviour {
+    /*We really should have started with this one instead of the Variables and Functions assignments.
+     * It just would have been useful to know the multiline comment command for these fifty word
+     * essays. Also it seems to be the most basic of basics to start with. I hate to complain but
+     * it really would have been nice to start off with this.
+     * */
+    private bool SpaceAliens = true;
+
+	// Use this for initialization
+	void Start () {
+        if (SpaceAliens = true)
+        {
+            print("I knew it!!");
+        }
+	}
+	/*
+	// Update is called once per frame
+	void Update () {
+	if(SpaceAliens = false)
+        {
+            print("Gosh darn it!!")
+        }
+	}
+    */
+}
