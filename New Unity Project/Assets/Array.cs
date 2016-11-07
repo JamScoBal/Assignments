ca4bdf3c39f4abeaad2959f0ad912b33fe0bde16
Unknown <jameson.ballard21@gmail.com>
2016-11-06 20:14:41 -0700

    List & Arrays

---
+++ New Unity Project/Assets/Array.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Array : MonoBehaviour {
+
+    bool[] Soup = {true, false, true};
+
+	// Use this for initialization
+	void Start () {
+        List<ListScript> list = new List<ListScript>();
+
+        list.Add(new ListScript("Cream of Chicken"));
+        list.Add(new ListScript("Cream of Mushroom"));
+        list.Add(new ListScript("Tomato"));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ New Unity Project/Assets/List.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ListScript : IComparable<ListScript>
+{
+    /* Arrays are there to make it so you do not have to write out a whole bunch of similar variables in one script.
+     * A list is basically the same thing except that you can do a whole lot more with the variables. What extra you
+     * can do with them is a mystery to me but I know that it is possible to do more with Lists. Arrays can be applied
+     * to gameObjects as well where it will ask in the interface for a size and the number you put in will represent
+     * the number of objects you drag and drop in the list to be referenced.*/
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
